157c0693e230c615fb3bc02f1fceb3881b4a9296
Marat Elagin <elagin@mindbox.cloud>
2024-07-10 18:43:56 +0300

    *

--- Ollivanders.Services/Database/MagesRepository.cs
+++ Ollivanders.Services/Database/MagesRepository.cs
@@ -4,21 +4,11 @@ namespace Ollivanders.Services.Database;
 
 public sealed class MagesRepository
 {
-    private readonly DatabaseContext _databaseContext;
     private readonly DbSet<Mage> _set;
 
     public MagesRepository(DatabaseContext databaseContext)
     {
-        _databaseContext = databaseContext;
         _set = databaseContext.Set<Mage>();
-    }
-
-    public async Task<Mage> CreateAsync(Mage magicWand)
-    {
-        await _set.AddAsync(magicWand);
-        await _databaseContext.SaveChangesAsync();
-
-        return magicWand;
     }
     
     public async Task<Mage?> TryGetByIdAsync(int id)
--- Ollivanders/obj/Debug/net8.0/Ollivanders.AssemblyInfo.cs
+++ Ollivanders/obj/Debug/net8.0/Ollivanders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ollivanders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fa3e324cc3364488a79824150d5c57ee05642a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ollivanders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ollivanders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
