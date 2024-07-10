be53c6a934457db873d484debb250b5fa85495ac
Akim Adzhi-Asan <adzhiasan.a@mindbox.cloud>
2024-07-10 21:11:43 +0300

    ,

--- Ollivanders.Services/Repair/RepairRequest.cs
+++ Ollivanders.Services/Repair/RepairRequest.cs
@@ -4,7 +4,7 @@ namespace Ollivanders.Repair;
 
 public class RepairRequest
 {
-    // составной ключ
+    // составной ключ по идентификатору палочки и парту
     public int MagicWandId { get; set; }
     public WandPart Part { get; set; }
 }
